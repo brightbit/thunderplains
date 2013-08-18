6597d64ba93cf68b03ec555fb7a9e237b331fad8
Joshua Ogle <joshua@joshuaogle.com>
2013-08-18 00:07:43 -0500

    smooth scrolling, scroll hint for mobile

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,5 +1,20 @@
 $(function() {
 
+  // Smooth Scroll
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+         $('html,body').animate({
+           scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 });
 
 // Map
